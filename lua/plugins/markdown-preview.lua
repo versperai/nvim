2d08722519d32ab618371ce6c6659f7d61e90ec3
BoHao Chen <chenbohao@stu.xupt.edu.cn>
2026-06-23 14:17:28 +0800

    fix: auto (cd ~/.local/share/nvim/lazy/markdown-preview.nvim/app && npm install)

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -2,9 +2,7 @@ return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+  build = "cd app && npm install",
   keys = {
     { "<leader>cp", "<cmd>MarkdownPreviewToggle<CR>", desc = "Markdown Preview Toggle" },
   },
